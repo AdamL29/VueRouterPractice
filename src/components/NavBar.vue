c23c46757b4ebbd0082762bef9ebe0d4fa7c4a02
Adam <adamlederer@live.ca>
2022-12-08 20:42:21 -0700

    finished first router project

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <router-link to="/">Home</router-link> 
+        <!-- router-link to="path that is used in VIEWS vue files" -->
+        <router-link to="/contact-us">Contact</router-link>
+        <!-- router-link is translated as <a></a> tags -->
+        <router-link to="/reservations">Reservations</router-link>
+        <a href="https://google.ca" target="_blank">Google</a>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "NavBar"
+    }
+</script>
+
+<style scoped>
+/* .router-link-active {
+
+}*/
+a{
+    /* class seen in webpage inspector */
+    margin-right: 10px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,98 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MenuPage from '@/views/MenuPage'; // Or '@/views/MenuPage.vue';
+import ContactPage from '@/views/ContactPage';
+import ReservationPage from '@/views/ReservationPage';
 
 Vue.use(VueRouter)
 
 const routes = [
+  // This section creates the path names that match the router-link tags.
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    component : MenuPage,
+    meta : [
+      {
+        title : 'Our Menu'
+        // The new page title with meta information
+      },
+      // Below is the meta information
+      {
+        name : "description",
+        content: "Enjoy our italian style pizza selection"
+      },
+      {
+        name : "Author",
+        content : "Timothy"
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path : "/contact-us",
+    component : ContactPage,
+    meta : [
+      {
+        title : 'Call Us'
+      },
+      {
+        name : "description",
+        content: "Call us 24/7"
+      },
+      {
+        name : "Author",
+        content : "Timothy"
+      }
+    ]
+  },
+  {
+    path : '/reservations',
+    component : ReservationPage,
+    meta : [
+      {
+        title : 'Make Reservations'
+      },
+      {
+        name : "description",
+        content: "Make a reservation for your next visit"
+      },
+      {
+        name : "Author",
+        content : "Timothy"
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to,from,next)=>{
+  // Get all Meta tags
+  let metaTags = document.querySelectorAll('meta');
+
+  // Loop through the tags and remove each.
+  for (let tag of metaTags){
+    tag.remove ();
+  }
+  let newTags = to.meta;
+  // Assign local variable and then place as innerText
+  document.querySelector('title').innerText = newTags[0].title; //.title used to title the page
+  
+  for (let i=1; i<newTags.length; i++){
+    document.querySelector('head').insertAdjacentHTML(`afterbegin`,
+          `<meta name="${newTags[i].name}" content="${newTags[i].content}">`)
+  }
+// This 
+// This is how to add the html meta tags needed!
+  document.querySelectorA(`head`).insertAdjacentHTML(`afterbegin`,`
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width,initial-scale=1.0">
+`)
+  to;
+  from;
+  next();
 })
 
 export default router
